e44a43d01dc2e83edbf00b8727a9dd0d3e8ebad1
Gean Bertani <geanbertani@gmail.com>
2020-12-21 14:50:49 -0400

    Inicialização do projeto com base nos estudos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+import Genres from './Genres';
+import GenresNew from './GenresNew';
+import GenresEdit from './GenresEdit';
+import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+
+const Home = () => {
+  return <h1>Home</h1>
+}
 
 function App() {
+  const [data, setData] = useState({})
+  useEffect(() => {
+    axios.get('/api').then(res => {
+      setData(res.data)
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Route path='/' exact component={Home} />
+        <Route path='/generos' exact component={Genres} />
+        <Route path='/generos/novo' exact component={GenresNew} />
+        <Route path='/generos/:id' exact component={GenresEdit} />
+      </div>
+    </Router>
   );
 }
 
